4b48371c6b50d8faebc64c89213214d8002ee78f
Curtis Chadwell <laurencechadwell@gmail.com>
2020-03-25 19:19:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(words)
-  split_string = words.split("")
+  split_string = words.split(" ")
   reversed = []
   split_string.each do |word|
     reversed << split_string.pop 
   end
-  reversed.join("")
+  reversed.join(" ")
   
 end
